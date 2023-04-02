4fd0fd3fb66582cb4a1c282e3f3597722ac9a4a7
Stefan Stölzle <stefan@stoelzle.me>
2023-04-02 10:49:54 +0200

    🔨 Refactor update script

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -1,23 +1,9 @@
 #!/usr/bin/env bash
 
-set -e
+set -eo pipefail
 
-# update function
-ncu() {
-  npx --no-install npm-check-updates -u
-  npm install --ignore-scripts
-}
-
-for D in packages/*; do
-  if [ -d "${D}" ]; then
-    pushd "${D}"
-      ncu
-    pushd
-  fi
-done
-
-ncu
-
+npx --no-install npm-check-updates -u -ws --root
+npm install --ignore-scripts -ws --include-workspace-root
 npm run format --if-present
 npm run build --if-present
 npm run test --if-present
