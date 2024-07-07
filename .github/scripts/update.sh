24bf66429aa07826d52833e23a51da8907268b10
Stefan Stölzle <stefan@stoelzle.me>
2024-07-07 09:35:56 +0200

    v6.0.0 (#213)

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -1,9 +1,24 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 set -eo pipefail
 
 npx --no-install npm-check-updates -u -ws --root
+
+# packages
+print -P "\n[ %5FUpdating packages%f ]"
+
 npm install --ignore-scripts -ws --include-workspace-root
 npm run format --if-present
 npm run build --if-present
 npm run test --if-present
+
+# archived packages
+pushd archived/vartastorage
+  print -P "\n[ %5FUpdating archived/vartastorage%f ]"
+
+  npx --no-install npm-check-updates -u
+  npm install --ignore-scripts
+  npm run format --if-present
+  npm run build --if-present
+  npm run test --if-present
+popd
--- .prettierrc.cjs
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  ...require('eslint-plugin-github/prettier.config'),
-  printWidth: 80
-}
