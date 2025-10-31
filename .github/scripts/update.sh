be636beea567ef91d82bd1a8a0a040f89b4490bf
Stefan Stölzle <stefan@stoelzle.me>
2025-10-31 08:05:59 +0100

    ⬆️ Simplify update script
    Use npx npm-check-updates and install 'packages' workspace

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -2,23 +2,12 @@
 
 set -eo pipefail
 
-npx --no-install npm-check-updates -u -ws --root
+npx npm-check-updates --upgrade --workspaces --root
 
 # packages
 print -P "\n[ %5FUpdating packages%f ]"
 
-npm install --ignore-scripts -ws --include-workspace-root
+npm install --ignore-scripts --workspace packages --include-workspace-root
 npm run format --if-present
 npm run build --if-present
 npm run test --if-present
-
-# archived packages
-pushd archived/vartastorage
-  print -P "\n[ %5FUpdating archived/vartastorage%f ]"
-
-  npx --no-install npm-check-updates -u
-  npm install --ignore-scripts
-  npm run format --if-present
-  npm run build --if-present
-  npm run test --if-present
-popd
